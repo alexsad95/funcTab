332aee775c6a8f22e55b9da86531a7d6d8b44e3c
alexsad95 <aleksandrsadchikov.17@gmail.com>
2021-01-26 17:09:39 +0200

    rm options and popup files

--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,7 @@ module.exports = {
     tab: {
       template: 'public/browser-extension.html',
       entry: './src/tab/main.js',
-      title: 'New Tab',
-    },
-    popup: {
-      template: 'public/browser-extension.html',
-      entry: './src/popup/main.js',
-      title: 'Popup',
-    },
-    options: {
-      template: 'public/browser-extension.html',
-      entry: './src/options/main.js',
-      title: 'Options',
+      title: 'funcTab',
     },
   },
   pluginOptions: {
